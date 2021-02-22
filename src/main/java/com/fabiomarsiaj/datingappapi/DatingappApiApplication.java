cdb1855c893d616573542ffd835cb305fb51586f
Fabio <fabiomarsiaj@gmail.com>
2021-02-22 12:50:59 -0300

    first commit

---
+++ src/main/java/com/fabiomarsiaj/datingappapi/DatingappApiApplication.java
@@ -0,0 +1,13 @@
+package com.fabiomarsiaj.datingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiomarsiaj/datingappapi/DatingappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomarsiaj.datingappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
